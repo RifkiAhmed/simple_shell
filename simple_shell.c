63e5d902e4327c168b8ce32c61cec82d1bbc9ffc
ahmed <rifkiahmed.91@gmail.com>
2023-05-16 23:12:45 +0000

    Program that display a prompt and wait for the user to type a command

---
+++ simple_shell.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - test code
+ * @ac: argument counter
+ * @av: array of string, each string is one of the arguments
+ *
+ * Return: 0
+ */
+int main(int ac, char **av)
+{
+	char *lineptr;
+	size_t n = 0;
+	int read_char;
+	(void)ac;
+	(void)av;
+
+	while(1)
+	{
+		write(STDOUT_FILENO, "IA $: ", 6);
+
+		read_char = getline(&lineptr, &n, stdin);
+		if (read_char != -1)
+			write(1, lineptr, read_char);
+		else
+			return (-1);
+	}
+	return (0);
+}
